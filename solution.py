ca7bc317ac3e8bdb192324df581b137497d8e807
Albert Dominguez Mantes <31998088+AlbertDominguez@users.noreply.github.com>
2024-07-23 14:39:24 +0200

    Changes to intro

--- solution.py
+++ solution.py
@@ -11,11 +11,12 @@
     </table>
 </div>
 
-In the following exercise we explore the basic building blocks of deep learning: the perceptron and how to stack multiple perceptrons together into layers to build a neural network. In particular, we will:
+In the following exercise we will explore the basic building blocks of deep learning: the perceptron and how to stack multiple perceptrons together into layers to build a neural network. We will also introduce convolutional neural networks (CNNs) for image classification.
+In particular, we will:
 - Implement a perceptron and a 2-layer perceptron to compute the XOR function using NumPy.
-- Be introduced to PyTorch, a popular deep learning framework.
-- Implement and train a simple neural network to classify points in a 2D plane using PyTorch.
-- Implement and train a simple convolutional neural network to classify hand-written digits from the MNIST dataset using PyTorch.
+- Introduce PyTorch, a popular framework for deep learning.
+- Implement and train a simple neural network (a multi-layer perceptron) to classify points in a 2D plane using PyTorch.
+- Implement and train a simple deep convolutional neural network to classify hand-written digits from the MNIST dataset using PyTorch.
 - Discuss important topics of ML/DL, such as data splitting, under/overfitting and model generalization.
 
 <div class="alert alert-block alert-danger">
