82ab7419a07a0a8609def536f86671cb09bfa5b7
Albert Dominguez Mantes <31998088+AlbertDominguez@users.noreply.github.com>
2024-07-23 14:36:20 +0200

    Switch to conda

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Create environment
-mamba create -y -n 02_intro_dl python=3.9
+conda create -y -n 02_intro_dl python=3.9
 
 # Activate environment
-mamba activate 02_intro_dl
+conda activate 02_intro_dl
 
 # Install dependencies
-mamba install -y matplotlib jupyter tqdm
-mamba install -y pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install -y matplotlib jupyter tqdm
+conda install -y pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 
-mamba deactivate
+conda deactivate
 
