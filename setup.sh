9cee84661ea41d8a39cb23837bacb311593af8e1
AlbertDominguez <albert.dominguezmantes@epfl.ch>
2025-08-21 12:00:16 -0400

    fix setup failing if specific env names exist in system

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ conda create -y -n 01_intro_dl python=3.11
 conda install -y matplotlib jupyter tqdm --name 01_intro_dl
 
 # Install PyTorch
-ENV_PATH=$(conda info --envs | grep 01_intro_dl | awk '{print $NF}')
+ENV_PATH=$(conda info --base)/envs/01_intro_dl
 $ENV_PATH/bin/pip install "torch"
 $ENV_PATH/bin/pip install "torchvision"
 
