0019b7c69bf49ff9dd6ce7b1eaf787c12204ccb0
AlbertDominguez <albert.dominguezmantes@epfl.ch>
2025-08-21 12:04:42 -0400

    bump setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+ENV_NAME="01_intro_dl"
+
 # Create environment
-conda create -y -n 01_intro_dl python=3.11
+conda create -y -n $ENV_NAME python=3.11
 
 # Install dependencies
 conda install -y matplotlib jupyter tqdm --name 01_intro_dl
 
 # Install PyTorch
-ENV_PATH=$(conda info --base)/envs/01_intro_dl
+ENV_PATH=$(conda info --base)/envs/$ENV_NAME
 $ENV_PATH/bin/pip install "torch"
 $ENV_PATH/bin/pip install "torchvision"
 
